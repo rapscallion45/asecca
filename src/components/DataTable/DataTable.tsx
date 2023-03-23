841ed021b1a3b2893ac2c37928c6c3fcda13066a
Carl <carl.scrivener.89@gmail.com>
2023-03-23 23:08:05 +0000

    refactor table row into seperate component

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, Fragment, ChangeEvent } from 'react';
+import { FC, Fragment } from 'react';
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -10,12 +10,12 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
-import { Input, InputAdornment, FormControl } from '@mui/material';
 import ErrorIcon from '@mui/icons-material/Error';
+import DataTableRow from './DataTableRow/DataTableRow';
 import ClientOnly from '../ClientOnly/ClientOnly';
 
-/* table cell stylings */
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+/* table head cell stylings */
+const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
   /* table head colors */
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
@@ -26,129 +26,6 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     fontSize: 14,
   },
 }));
-
-/* table row stylings */
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  /* alternate row background colors */
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  /* hide last border */
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
-
-interface NonEditableCellProps {
-  column: any;
-  row: any;
-  editValue: string;
-}
-
-/* Non Editable Table Cell helper component */
-/* ======================================== */
-const NonEditableCell: FC<NonEditableCellProps> = (props) => {
-  const { row, column, editValue } = props;
-
-  return (
-    <StyledTableCell align="center">
-      {/* all values are currency apart from name */}
-      {column.key !== 'name' && column.label !== 'Prevailing' ? (
-        <div>
-          {row[column.key] != null
-            ? `£${parseInt(row[column.key], 10).toFixed(2)}`
-            : '--'}
-        </div>
-      ) : (
-        <>
-          {/* the 'Prevailing' column is always equal to the
-          editable col (permission level) */}
-          {column.label === 'Prevailing'
-            ? `£${parseInt(editValue, 10).toFixed(2)}` || '--'
-            : row[column.key]}
-        </>
-      )}
-    </StyledTableCell>
-  );
-};
-
-interface EditableCellProps {
-  column: any;
-  row: any;
-  editValue: string;
-  handleEditValueChange: (
-    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) => void;
-}
-
-/* Editable Table Cell helper component */
-/* ==================================== */
-const EditableCell: FC<EditableCellProps> = (props) => {
-  const { row, column, editValue, handleEditValueChange } = props;
-
-  return (
-    <StyledTableCell sx={{ textAlign: 'center', p: 0 }}>
-      <FormControl sx={{ m: 1 }} variant="standard">
-        <Input
-          id={`${row.name}-${column.key}-input`}
-          startAdornment={<InputAdornment position="start">£</InputAdornment>}
-          onChange={handleEditValueChange}
-          value={editValue}
-          required
-        />
-      </FormControl>
-    </StyledTableCell>
-  );
-};
-
-interface DataTableRowProps {
-  columns: any;
-  row: any;
-  editCol: any;
-}
-
-/* Data Table Row helper component */
-/* =============================== */
-const DataTableRow: FC<DataTableRowProps> = (props) => {
-  const { row, columns, editCol } = props;
-  const [editValue, setEditValue] = useState<string>(
-    parseInt(row[editCol?.key], 10).toFixed(2)
-  );
-
-  const handleValueChange = (
-    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) => {
-    setEditValue(event.target.value);
-  };
-
-  return (
-    <StyledTableRow>
-      {/* map passed column data for current row */}
-      {columns.map((column: any) => (
-        <Fragment key={`${row.name}-${column.key}`}>
-          {/* render a normal cell if not editable */}
-          {column.label !== editCol.label && (
-            <NonEditableCell
-              row={row}
-              column={column}
-              /* we need the edit value for 'Prevailing' */
-              editValue={editValue}
-            />
-          )}
-          {/* if this is col is editable, render input cell */}
-          {column.label === editCol.label && (
-            <EditableCell
-              row={row}
-              column={column}
-              editValue={editValue}
-              handleEditValueChange={handleValueChange}
-            />
-          )}
-        </Fragment>
-      ))}
-    </StyledTableRow>
-  );
-};
 
 interface DataTableProps {
   name: string;
@@ -180,9 +57,9 @@ const DataTable: FC<DataTableProps> = (props) => {
             <TableRow>
               {/* map passed columns to table headers row */}
               {columns.map((column: any) => (
-                <StyledTableCell key={column.key} align="center">
+                <StyledTableHeadCell key={column.key} align="center">
                   {column.label}
-                </StyledTableCell>
+                </StyledTableHeadCell>
               ))}
             </TableRow>
           </TableHead>
