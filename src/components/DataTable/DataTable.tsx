cfc487f4bf4eb015630bfd2a484900f66237daf7
Carl <carl.scrivener.89@gmail.com>
2023-03-24 19:02:41 +0000

    refactor data table loading and header rows

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,30 +1,15 @@
 import { FC, Fragment } from 'react';
-import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import CircularProgress from '@mui/material/CircularProgress';
-import Typography from '@mui/material/Typography';
 import DataTableRow from './DataTableRow/DataTableRow';
 import ClientOnly from '../ClientOnly/ClientOnly';
 import DataTableErrorRow from './DataTableErrorRow/DataTableErrorRow';
+import DataTableLoadingRow from './DataTableLoadingRow/DataTableLoadingRow';
+import DataTableHeader from './DataTableHeader/DataTableHeader';
 import { DataTableColumn } from '../types';
-
-/* table head cell stylings */
-const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
-  /* table head colors */
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.secondary.main,
-    color: theme.palette.primary.contrastText,
-    fontSize: 16,
-    fontWeight: 400,
-  },
-}));
 
 interface DataTableProps {
   name: string;
@@ -53,14 +38,7 @@ const DataTable: FC<DataTableProps> = (props) => {
       <TableContainer component={Paper}>
         <Table aria-label={`${name} table`}>
           <TableHead>
-            <TableRow>
-              {/* map passed columns to table headers row */}
-              {columns.map((column: DataTableColumn) => (
-                <StyledTableHeadCell key={column.key} align="center">
-                  {column.label}
-                </StyledTableHeadCell>
-              ))}
-            </TableRow>
+            <DataTableHeader columns={columns} />
           </TableHead>
           <TableBody>
             <>
@@ -88,21 +66,7 @@ const DataTable: FC<DataTableProps> = (props) => {
                 />
               )}
               {isLoading && (
-                <TableRow>
-                  <TableCell colSpan={columns.length}>
-                    <Box
-                      display="flex"
-                      flexDirection="column"
-                      py={7}
-                      alignItems="center"
-                    >
-                      <CircularProgress size={30} />
-                      <Typography variant="body1" mt={2}>
-                        Loading...
-                      </Typography>
-                    </Box>
-                  </TableCell>
-                </TableRow>
+                <DataTableLoadingRow columns={columns} message="Loading..." />
               )}
             </>
           </TableBody>
---
+++ src/components/DataTable/DataTableHeader/DataTableHeader.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import { styled } from '@mui/material/styles';
+import TableRow from '@mui/material/TableRow';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import { DataTableColumn } from '@/components/types';
+
+/* table header cell stylings */
+const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
+  /* table header colors */
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.primary.contrastText,
+    fontSize: 16,
+    fontWeight: 400,
+  },
+}));
+
+interface DataTableHeaderProps {
+  columns: Array<DataTableColumn>;
+}
+
+/* Data Table Header helper */
+/* ======================== */
+const DataTableHeader: FC<DataTableHeaderProps> = (props) => {
+  const { columns } = props;
+
+  return (
+    <TableRow>
+      {/* map passed columns to table headers row */}
+      {columns.map((column: DataTableColumn) => (
+        <StyledTableHeadCell key={column.key} align="center">
+          {column.label}
+        </StyledTableHeadCell>
+      ))}
+    </TableRow>
+  );
+};
+
+export default DataTableHeader;
