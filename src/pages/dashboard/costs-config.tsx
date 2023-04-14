ed9765adbea2f9955a498109d488a88e49a42d0c
Carl <carl.scrivener.89@gmail.com>
2023-04-14 13:00:31 +0100

    update comment style

--- src/pages/dashboard/costs-config.tsx
+++ src/pages/dashboard/costs-config.tsx
@@ -9,30 +9,34 @@ import { setPermissionLevel } from '@/redux/slices/userPermissionSlice';
 import { fetchBySourceId as fetchCostsConfigBySourceId } from '@/redux/slices/costsConfigSlice';
 import CostsConfigTable from '@/components/CostsConfigTable/CostsConfigTable';
 
-/* Costs Config Test Page */
-/* ====================== */
+/**
+ * Costs Config Test Page
+ *
+ * Presents user with costs configuration table, with data fetched from Asecca
+ * API, based on passed page query params
+ */
 const CostsConfigTestPage: NextPageWithLayout = () => {
-  /* shorthand helper for dispatching redux actions */
+  /** shorthand helper for dispatching redux actions */
   const dispatch = useDispatch<AppDispatch>();
 
-  /* get user permission level held in redux state */
+  /** get user permission level held in redux state */
   const { permission } = useSelector((state: AppState) => state.userPermission);
 
-  /* copy of page query param held in local page state */
+  /** copy of page query param held in local page state */
   const [query, setQuery] = useState<string | (string | null)[]>('');
 
-  /* *** THIS IS A TEST PARAM - USER WILL NOT BE ABLE TO CHANGE PERMISSION *** */
-  /* keep a copy of the original API request permission level in local state */
+  /** *** THIS IS A TEST PARAM - USER WILL NOT BE ABLE TO CHANGE PERMISSION *** */
+  /** keep a copy of the original API request permission level in local state */
   const [apiPermission, setApiPermission] = useState<string>('');
 
-  /* get page query params from URL on first load, and set orig permission */
+  /** get page query params from URL on first load, and set orig permission */
   useEffect(() => {
-    /* check for customer, project or collection data query */
+    /** check for customer, project or collection data query */
     const { customer, project, collection } = queryString.parse(
       window.location.search
     );
 
-    /* test for which type of query this is and set state accordingly */
+    /** test for which type of query this is and set state accordingly */
     if (customer !== undefined && customer !== null && customer !== '') {
       /* set Customer permission level and data ID to be fetched from API */
       dispatch(setPermissionLevel({ level: 'Customer' }));
@@ -40,20 +44,20 @@ const CostsConfigTestPage: NextPageWithLayout = () => {
       setApiPermission('Customer');
     }
     if (project !== undefined && project !== null && project !== '') {
-      /* set Project permission level and data ID to be fetched from API */
+      /** set Project permission level and data ID to be fetched from API */
       dispatch(setPermissionLevel({ level: 'Project' }));
       setQuery(project);
       setApiPermission('Project');
     }
     if (collection !== undefined && collection !== null && collection !== '') {
-      /* set Collection permission level and data ID to be fetched from API */
+      /** set Collection permission level and data ID to be fetched from API */
       dispatch(setPermissionLevel({ level: 'Collection' }));
       setQuery(collection);
       setApiPermission('Collection');
     }
   }, [dispatch]);
 
-  /* whenever the page query is updated, fetch new data from API */
+  /** whenever the page query is updated, fetch new data from API */
   useEffect(() => {
     if (query) {
       dispatch(
@@ -72,12 +76,13 @@ const CostsConfigTestPage: NextPageWithLayout = () => {
           Costing Configuration - Lloyds Bank - {permission.level} {query}
         </Typography>
       </Box>
+      {/** load table with fetched data and permission level */}
       <CostsConfigTable permission={permission} query={query as string} />
     </>
   );
 };
 
-/* dashboard layout used for Costs Config page */
+/** dashboard layout used for Costs Config page */
 CostsConfigTestPage.Layout = DashboardLayout;
 
 export default CostsConfigTestPage;
