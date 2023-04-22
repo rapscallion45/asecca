172e4803d7cb3caf5f6782a58a54e75091880945
Carl <carl.scrivener.89@gmail.com>
2023-04-22 17:41:59 +0100

    add home page tests

---
+++ src/tests/pages/Home.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HomePage from '../../pages/index';
+
+/**
+ * Home Page Unit Tests
+ *
+ * @author Carl Scrivener {@link https://github.com/rapscallion45 GitHub}
+ * @since 0.0.0
+ */
+describe('Home Page', () => {
+  it('Renders correctly', async () => {
+    /** perform snapshot test */
+    const tree = renderer.create(<HomePage />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
