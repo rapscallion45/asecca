21b704e3107270a86fc0f52e2ab6592689d49e64
Carl <carl.scrivener.89@gmail.com>
2023-06-07 16:47:15 +0100

    add nav item tests

--- src/components/NavItem/NavItem.test.tsx
+++ src/components/NavItem/NavItem.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { waitFor, render, fireEvent, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import NavItem from './NavItem';
-import navConfig from '../DashboardSideBar/dashboardSideBarItems';
+import navItems from '../../../__mocks__/dashboardSideBarItemsMock';
 
 /* mock function for checking currently active nav item URI */
 const mockActivePathCheckCallback = (path: string | undefined) =>
@@ -18,9 +19,67 @@ describe('NavItem', () => {
     /** perform snapshot test */
     const tree = renderer
       .create(
-        <NavItem item={navConfig[1]} active={mockActivePathCheckCallback} />
+        <NavItem item={navItems[1]} active={mockActivePathCheckCallback} />
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  describe('Submenu', () => {
+    it('Should render dropdown list when item clicked', async () => {
+      /** Arrange */
+      /** Act */
+      render(
+        <NavItem item={navItems[11]} active={mockActivePathCheckCallback} />
+      );
+
+      /** Assert */
+      await waitFor(() => {
+        expect(
+          screen.getByText(navItems[11].title as string)
+        ).toBeInTheDocument();
+      });
+      expect(
+        screen.queryByText(navItems[11].children[0].title as string)
+      ).toBeNull();
+
+      /** Act - click nav item to open submenu */
+      fireEvent(
+        screen.getByText(navItems[11].title as string),
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+        })
+      );
+
+      /** Assert */
+      await waitFor(() => {
+        expect(
+          screen.getByText(navItems[11].children[0].title as string)
+        ).toBeInTheDocument();
+      });
+      expect(
+        screen.getByText(navItems[11].title as string)
+      ).toBeInTheDocument();
+
+      /** Act - click nav item again to close submenu */
+      fireEvent(
+        screen.getByText(navItems[11].title as string),
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+        })
+      );
+
+      /** Assert */
+      await waitFor(() => {
+        expect(
+          screen.queryByText(navItems[11].children[0].title as string)
+        ).toBeNull();
+      });
+      expect(
+        screen.getByText(navItems[11].title as string)
+      ).toBeInTheDocument();
+    });
   });
 });
