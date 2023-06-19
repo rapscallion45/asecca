fc565f4043be8eab3f7bb10b0395dcefa07beea3
Carl <carl.scrivener.89@gmail.com>
2023-06-19 16:52:22 +0100

    add Slice Context component

---
+++ src/components/SliceProvider/SliceProvider.tsx
@@ -0,0 +1,74 @@
+import { FC, ReactNode, createContext, useContext } from 'react';
+import { useSelector } from 'react-redux';
+import { AppState } from '@/redux/store';
+import { collectionsKanbanSlice } from '@/redux/slices/collectionsKanbanSlice';
+
+const SliceContext = createContext(collectionsKanbanSlice);
+
+/**
+ * Slice Provider Props
+ *
+ * @author Carl Scrivener {@link https://github.com/rapscallion45 GitHub}
+ * @since 0.0.8
+ *
+ * @typedef ISliceProviderProps
+ * @prop {any} slice - context slice to provide down to child nodes
+ * @prop {ReactNode} children - child nodes
+ */
+interface ISliceProviderProps {
+  slice: any;
+  children: ReactNode;
+}
+
+/**
+ * Slice Provider
+ *
+ * Slice provider functional component wrapper for providing global state
+ * managment functionality to child components
+ *
+ * @author Carl Scrivener {@link https://github.com/rapscallion45 GitHub}
+ * @since 0.0.8
+ *
+ * @component
+ * @param {ISliceProviderProps} props - component props
+ * @returns {FC} - slice provider functional component
+ */
+const SliceProvider: FC<ISliceProviderProps> = (props) => {
+  const { slice, children } = props;
+  return (
+    <SliceContext.Provider value={slice}>{children}</SliceContext.Provider>
+  );
+};
+
+/**
+ * Provider specific actions interface for passed slice
+ *
+ * @author Carl Scrivener {@link https://github.com/rapscallion45 GitHub}
+ * @since 0.0.8
+ * @memberof KanbanReduxSlice
+ *
+ * @see See [more info on Redux Slice creation](https://redux-toolkit.js.org/api/createSlice)
+ *
+ * @constant
+ * @type {CaseReducerActions<any, string>}
+ */
+export const useSliceActions = () => useContext(SliceContext).actions;
+
+/**
+ * Provider specific selector interface for passed slice
+ *
+ * @author Carl Scrivener {@link https://github.com/rapscallion45 GitHub}
+ * @since 0.0.8
+ * @memberof KanbanReduxSlice
+ *
+ * @see See [more info on Redux Slice creation](https://redux-toolkit.js.org/api/createSlice)
+ *
+ * @constant
+ * @type {AppState}
+ */
+export const useSliceSelector = () => {
+  const { name } = useContext(SliceContext);
+  return useSelector((state: AppState) => state[name as keyof AppState]);
+};
+
+export default SliceProvider;
