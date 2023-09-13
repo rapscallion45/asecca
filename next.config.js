58dd469cd2144ddbae4e46dfd52d4e83b8f47372
Carl <carl.scrivener.89@gmail.com>
2023-09-13 09:45:08 +0100

    tody & comment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /** output standalone build for integration with Asecca server */
+  output: 'standalone',
   /** env variables must be mirrored in .env.test for running Jest */
   env: {
     NEXT_PUBLIC_APP_NAME: `Asecca`,
     NEXT_PUBLIC_APP_DESCRIPTION: `Asecca front end client SPA`,
   },
-  output: 'standalone',
 };
 
 module.exports = nextConfig;
