eacd4d90be34dad8b6ebcd6e86a7f40ea13be865
Carl <carl.scrivener.89@gmail.com>
2023-11-08 18:29:27 +0000

    add Kanban Board tests

---
+++ src/components/KanbanBoard/KanbanBoard.test.tsx
@@ -0,0 +1,301 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, screen, waitFor, act } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { IKanbanBoardColumn } from '@/lib/api/api-types';
+import {
+  collectionsKanbanSlice,
+  fetchByProjectId as fetchKanbanBoardByProjectId,
+} from '@/redux/slices/collectionsKanbanSlice';
+import store from '../../redux/store';
+import KanbanBoard from './KanbanBoard';
+import collectionsKanbanDataMock from '../../../__mocks__/Kanban/collectionsKanbanDataMock';
+import SliceProvider from '../SliceProvider/SliceProvider';
+
+/* default test query ID */
+const query: string = '66135000001760012';
+
+/* mock Client Only renderer - just render children */
+jest.mock('../ClientOnly/ClientOnly', () => (props: any) => props.children);
+
+/**
+ * Kanban Board Unit Tests
+ *
+ * @author Carl Scrivener {@link https://github.com/rapscallion45 GitHub}
+ * @since 0.0.22
+ */
+describe('Kanban Board', () => {
+  describe('Values', () => {
+    /** ensure mock function calls are cleared after test */
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('Should render expected columns and number of tasks for passed data', async () => {
+      /** Arrange */
+      act(() => {
+        store.dispatch(
+          fetchKanbanBoardByProjectId({
+            projectId: query,
+          })
+        );
+      });
+
+      /** Act */
+      act(() => {
+        render(
+          <Provider store={store}>
+            <SliceProvider slice={collectionsKanbanSlice}>
+              <KanbanBoard
+                currentData={collectionsKanbanDataMock.collections}
+              />
+            </SliceProvider>
+          </Provider>
+        );
+      });
+
+      /** Assert - correct value should be displayed from test data */
+      await waitFor(() => {
+        collectionsKanbanDataMock.collections.columns.forEach(
+          (col: IKanbanBoardColumn) => {
+            expect(
+              screen.getByText(
+                `${col.name} (${col.tasks.length + (col.groups?.length || 0)})`
+              )
+            ).toBeInTheDocument();
+          }
+        );
+      });
+    });
+
+    it('Should render expected columns and number of tasks only if groups hidden', async () => {
+      /** Arrange */
+      act(() => {
+        store.dispatch(
+          fetchKanbanBoardByProjectId({
+            projectId: query,
+          })
+        );
+      });
+
+      /** Act */
+      act(() => {
+        render(
+          <Provider store={store}>
+            <SliceProvider slice={collectionsKanbanSlice}>
+              <KanbanBoard
+                currentData={collectionsKanbanDataMock.collections}
+                hideGroups
+              />
+            </SliceProvider>
+          </Provider>
+        );
+      });
+
+      /** Assert - correct value should be displayed from test data */
+      await waitFor(() => {
+        collectionsKanbanDataMock.collections.columns.forEach(
+          (col: IKanbanBoardColumn) => {
+            expect(
+              screen.getByText(`${col.name} (${col.tasks.length})`)
+            ).toBeInTheDocument();
+          }
+        );
+      });
+    });
+
+    it('Should render expected columns and number of groups only if tasks hidden', async () => {
+      /** Arrange */
+      act(() => {
+        store.dispatch(
+          fetchKanbanBoardByProjectId({
+            projectId: query,
+          })
+        );
+      });
+
+      /** Act */
+      act(() => {
+        render(
+          <Provider store={store}>
+            <SliceProvider slice={collectionsKanbanSlice}>
+              <KanbanBoard
+                currentData={collectionsKanbanDataMock.collections}
+                hideTasks
+              />
+            </SliceProvider>
+          </Provider>
+        );
+      });
+
+      /** Assert - correct value should be displayed from test data */
+      await waitFor(() => {
+        collectionsKanbanDataMock.collections.columns.forEach(
+          (col: IKanbanBoardColumn) => {
+            expect(
+              screen.getByText(`${col.name} (${col.groups?.length || 0})`)
+            ).toBeInTheDocument();
+          }
+        );
+      });
+    });
+
+    it('Should render Save and Reset Board buttons if edit mode enabled', async () => {
+      /** Arrange */
+      act(() => {
+        store.dispatch(
+          fetchKanbanBoardByProjectId({
+            projectId: query,
+          })
+        );
+      });
+
+      /** Act */
+      act(() => {
+        render(
+          <Provider store={store}>
+            <SliceProvider slice={collectionsKanbanSlice}>
+              <KanbanBoard
+                currentData={collectionsKanbanDataMock.collections}
+                canEdit
+              />
+            </SliceProvider>
+          </Provider>
+        );
+      });
+
+      /** Assert - correct value should be displayed from test data */
+      await waitFor(() => {
+        expect(screen.getByText('Save')).toBeInTheDocument();
+      });
+      expect(screen.getByText('Reset Board')).toBeInTheDocument();
+    });
+
+    it('Should not render Save and Reset Board buttons if edit mode enabled', async () => {
+      /** Arrange */
+      act(() => {
+        store.dispatch(
+          fetchKanbanBoardByProjectId({
+            projectId: query,
+          })
+        );
+      });
+
+      /** Act */
+      act(() => {
+        render(
+          <Provider store={store}>
+            <SliceProvider slice={collectionsKanbanSlice}>
+              <KanbanBoard
+                currentData={collectionsKanbanDataMock.collections}
+                canEdit={false}
+              />
+            </SliceProvider>
+          </Provider>
+        );
+      });
+
+      /** Assert - correct value should be displayed from test data */
+      await waitFor(() => {
+        expect(screen.queryByText('Save')).toBeNull();
+      });
+      expect(screen.queryByText('Reset Board')).toBeNull();
+    });
+  });
+
+  describe('Error and Loading', () => {
+    /** ensure mock function calls are cleared after test */
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('Should render loading skeleton when loading prop set', async () => {
+      /** Arrange */
+      act(() => {
+        store.dispatch(
+          fetchKanbanBoardByProjectId({
+            projectId: query,
+          })
+        );
+      });
+
+      /** Act */
+      act(() => {
+        render(
+          <Provider store={store}>
+            <SliceProvider slice={collectionsKanbanSlice}>
+              <KanbanBoard
+                currentData={store.getState().collectionsKanban.data}
+              />
+            </SliceProvider>
+          </Provider>
+        );
+      });
+
+      /** Assert - loading skeleton should be rendered while API fetches data */
+      await waitFor(() => {
+        expect(
+          screen.getByTestId('kanban-board-loading-skeleton')
+        ).toBeInTheDocument();
+      });
+    });
+
+    it('Should render error message when error prop set', async () => {
+      /** Arrange */
+      const errorMsg: string =
+        'There is no data available for the selected Kanban Board.';
+      /** mock kanban services API to return error */
+      jest.mock('../../services/kanban/collectionsKanbanService', () => ({
+        getKanbanBoardByProjectId: async () => Promise.reject(new Error()),
+      }));
+      act(() => {
+        store.dispatch(
+          fetchKanbanBoardByProjectId({
+            projectId: query,
+          })
+        );
+      });
+
+      /** Act */
+      act(() => {
+        render(
+          <Provider store={store}>
+            <SliceProvider slice={collectionsKanbanSlice}>
+              <KanbanBoard
+                currentData={store.getState().collectionsKanban.data}
+              />
+            </SliceProvider>
+          </Provider>
+        );
+      });
+
+      /** Assert - correct value should be displayed from test data */
+      await waitFor(() => {
+        expect(screen.getByText(errorMsg)).toBeInTheDocument();
+      });
+    });
+  });
+
+  it('Renders correctly', async () => {
+    /** Arrange */
+    act(() => {
+      store.dispatch(
+        fetchKanbanBoardByProjectId({
+          projectId: query,
+        })
+      );
+    });
+
+    /** perform snapshot test */
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <SliceProvider slice={collectionsKanbanSlice}>
+            <KanbanBoard currentData={collectionsKanbanDataMock.collections} />
+          </SliceProvider>
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
